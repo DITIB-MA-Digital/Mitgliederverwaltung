ab36bd3d11cf710ee78e3613c624b1114e174ea2
can.porazan <can.porazan@isd.de>
2020-10-13 01:45:50 +0200

    extends init db query due to member entity changes

--- Mitgliederverwaltung/src/main/resources/db/migration/V1.0__init_db.sql
+++ Mitgliederverwaltung/src/main/resources/db/migration/V1.0__init_db.sql
@@ -1,10 +1,25 @@
-CREATE SCHEMA if not exist `mitgliederverwaltung`;
+CREATE SCHEMA IF NOT EXISTS `mitgliederverwaltung`;
 
-use mitgliederverwaltung;
+use `mitgliederverwaltung`;
 
 CREATE TABLE `mitgliederverwaltung`.`users` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `mitgliederverwaltung`.`members` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `member_id` INT(11),
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `birth_day` DATE NOT NULL,
+  `street` VARCHAR(45) NOT NULL,
+  `city` VARCHAR(45) NOT NULL,
+  `country` VARCHAR(45) NOT NULL,
+  `postal_code` INT(5) NOT NULL,
+  `is_approved` BOOLEAN NOT NULL DEFAULT 0,
+  `member_since` DATE NOT NULL,
   PRIMARY KEY (`id`));
